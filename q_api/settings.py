89bcd3323b8397993749a828f4ecfd62a307686a
unknown <nick.maina1@gmail.com>
2018-10-21 20:39:22 +0300

    Add dependancies

--- q_api/settings.py
+++ q_api/settings.py
@@ -68,16 +68,25 @@ WSGI_APPLICATION = 'q_api.wsgi.application'
 #
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'quickie',
-        'USER': 'postgres',
-        'PASSWORD': '1325Naftal',
-        'HOST': '',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "TEST": {"NAME": "testdatabase"},
     }
 }
 
+if not DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'quickie',
+            'USER': 'postgres',
+            'PASSWORD': '1325Naftal',
+            'HOST': '',
+            'PORT': '5432',
+        }
+    }
+    
 
 # DATABASES = {
 # 'default': {
