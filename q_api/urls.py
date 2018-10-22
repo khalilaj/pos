399aba0a0123f2562194bf2ba5c2c8353cd38c78
unknown <nick.maina1@gmail.com>
2018-10-22 17:41:56 +0300

    csrf except

--- q_api/urls.py
+++ q_api/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = [
-    url(r"^graphql/", GraphQLView.as_view(graphiql=True)),
+    url(r"^graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
     url(r"^admin/", admin.site.urls),
     url(r"^", include("q_pos.urls")),
 ]
